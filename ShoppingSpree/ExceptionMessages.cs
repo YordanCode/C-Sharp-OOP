a65f9dd658410e68e0466e84d4dea12567bf61b8
Yordan Georgiev <145832388+YordanCode@users.noreply.github.com>
2023-10-18 09:56:09 +0300

    Delete ShoppingSpree directory

--- ShoppingSpree/ExceptionMessages.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShoppingSpree;
-
-public static class ExceptionMessages
-{
-    public const string NameEmpty = "Name cannot be empty"; 
-    public const string MoneyNegative = "Money cannot be negative";
-}
