969123937b94ebe0095ffc343ce3762da1951dcd
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-11-12 18:24:57 +0200

    Exceptions and error handling solutions

---
+++ ExceptionsAndErrorHandling/SquareRoot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+try
+{
+    int number = int.Parse(Console.ReadLine());
+    if (number < 0)
+    {
+        throw new InvalidOperationException("Invalid number.");
+    }
+    else
+    {
+        int squareRoot = (int)Math.Sqrt(number);
+        Console.WriteLine(squareRoot);
+    }
+}
+catch (FormatException)
+{
+    throw new FormatException("Invalid number.");
+}
+catch (InvalidOperationException)
+{
+    throw new InvalidOperationException("Invalid number.");
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
+
