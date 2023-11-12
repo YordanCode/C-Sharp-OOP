02154e11e184b769df30919e39ad2a648ef05035
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-11-12 18:15:52 +0200

    Exceptions and error handling solutions

---
+++ ExceptionsAndErrorHandling/EnterNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+List<int> numbers = new();
+void ReadNumbers(int start, int end)
+{
+    try
+    {
+        int number = int.Parse(Console.ReadLine());
+        if (number > start && number < end)
+            numbers.Add(number);
+        else
+            throw new InvalidOperationException($"Your number is not in range {start} - 100!");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid Number!");
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+int start = 1;
+while (numbers.Count < 10)
+{
+    if (numbers.Count > 0)
+        start = numbers[numbers.Count - 1];
+    ReadNumbers(start, 100);
+}
+Console.WriteLine(string.Join(", ", numbers));
+
+    
+
