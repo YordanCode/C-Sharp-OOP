eeb6fddbb1b16f9f949ea6627932e2d346bd3624
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-11-12 19:21:18 +0200

    Exceptions and error handling solutions

---
+++ ExceptionsAndErrorHandling/SumOfIntegers/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int sum = 0;
+
+        string[] elements = input.Split();
+
+        foreach (string element in elements)
+        {
+            try
+            {
+                sum += int.Parse(element);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"The element '{element}' is in wrong format!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"The element '{element}' is out of range!");
+            }
+            finally
+            {
+                Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+            }
+        }
+
+        Console.WriteLine($"The total sum of all integers is: {sum}");
+    }
+}
+
