5a98bf03f6c505242a217c46ae7b9da496e3ef73
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-11-12 15:40:39 +0200

    Exceptions and error handling solutions

---
+++ ExceptionsAndErrorHandling/Cards/Card.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Cards
+{
+    public class Card
+    {
+        private const string playingCardFaces = "2345678910JQKA";
+        private const string playingCardSuits = "SHDC";
+        private string face;
+        private string suit;
+        public Card(string face, string suit)
+        {
+            Face = face;
+            Suit = suit;
+        }
+
+        public string Face
+        {
+            get => face;
+            set
+            {
+                if (playingCardFaces.Contains(value))
+                    face = value;
+                else
+                    throw new ArgumentException("Invalid card!");
+            }
+        }
+        public string Suit
+        {
+            get => suit;
+            set
+            {
+                if (playingCardSuits.Contains(value))
+                    suit = value;
+                else
+                    throw new ArgumentException("Invalid card!");
+            }
+        }
+
+        public override string ToString()
+        {
+            GetSuitSymbol();
+            return $"[{Face}{Suit}]";
+        }
+        private void GetSuitSymbol()
+        {
+            if (Suit == "S")
+                suit = "\u2660";
+            else if (Suit == "H")
+                suit = "\u2665";
+            else if (Suit == "D")
+                suit = "\u2666";
+            else if (Suit == "C")
+                suit = "\u2663";
+        }
+    }
+}
+
+
---
+++ ExceptionsAndErrorHandling/Cards/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cards
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Card> cards = new List<Card>();
+            string[] cardsArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < cardsArgs.Length; i++)
+            {
+                string[] cardFaceAndSuit = cardsArgs[i].Split(" ");
+                string cardFace = cardFaceAndSuit[0];
+                string cardSuit = cardFaceAndSuit[1];
+                try
+                {
+                    Card card = new Card(cardFace, cardSuit);
+                    card.Suit = "L";
+                    cards.Add(card);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            foreach (var card in cards)
+            {
+                Console.Write(card.ToString() + " ");
+            }     
+        }
+    }
+}
